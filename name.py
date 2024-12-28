0efac7a408f6c6829c06c88a5b36607cde098c1b
tammy-245678 <abdullahbigman38@gmail.com>
2024-12-28 21:14:10 +0500

    commit

---
+++ name.py
@@ -0,0 +1,12 @@
+
+import turtle
+# creating canvas
+turtle.Screen().bgcolor("Orange")
+# turtle object creation
+pen = turtle.Turtle()
+# creating a square
+for i in range(4): #i = 0,3
+ pen.forward(100)
+ pen.left(90)
+ i = i+1
+turtle.done()
